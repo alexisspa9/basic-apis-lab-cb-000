cb24b4a1a2651db84a7a19672a65529037d2cdee
Alexander Spanos <alexisspa9@gmail.com>
2018-04-13 07:42:39 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,7 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    client_id = "REPLACE ME"
+    client_id = "d506e0c75e4b59ce01fb56946f2b14002f3338db"
     secret = "REPLACE ME"
 
     resp = Faraday.get("https://api.github.com/search/repositories") do |req|
